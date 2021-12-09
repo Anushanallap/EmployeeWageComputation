67ef7a3d8eb4f6d827c83f3fe6ef7bc9347e683d
Anushanallap <anusha.n468@gmail.com>
2021-12-09 12:11:43 +0530

    add EmpUC5

--- EmpWageBuilderUC4.java
+++ EmpWageBuilderUC4.java
@@ -4,9 +4,6 @@ class EmpWageBuilderUC4
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 
-
-
-
 public static void main(String[] args){
 
         int empHrs = 0;
@@ -14,14 +11,14 @@ public static void main(String[] args){
 
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         switch (empCheck) {
-	case IS_PART_TIME;
-	empHrs =  4;
-	break;
-	case IS_FULL_TIME;
-        empHrs =  8;
-	break;
+		case IS_PART_TIME:
+		empHrs =  4;
+		break;
+	case IS_FULL_TIME:
+        	empHrs =  8;
+		break;
 	default:
-	empHrs = 0;
+		empHrs = 0;
 }
          empWage = empHrs * EMP_RATE_PER_HOUR;
 
